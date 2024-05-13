c60e63f27bb72979433d58c87b754dbd89d9ebe2
DylanJinx <2636353989@qq.com>
2024-05-13 17:53:47 +0800

    5.deref

--- vector_implementation/src/main.rs
+++ vector_implementation/src/main.rs
@@ -1,20 +1,22 @@
 use std::ptr::{self, NonNull};
 use std::mem;
 use std::alloc::{self, Layout};
+use std::ops::{Deref, DerefMut};
 
-pub struct Vec<T> {
+#[derive(Debug)]
+pub struct MyVec<T> {
     ptr: NonNull<T>, //指向分配的指针
     len: usize, // 已经初始化的元素个数
     cap: usize, // 分配的内存空间大小
 }
 
-unsafe impl<T: Send> Send for Vec<T> {}
-unsafe impl<T: Sync> Sync for Vec<T> {}
+unsafe impl<T: Send> Send for MyVec<T> {}
+unsafe impl<T: Sync> Sync for MyVec<T> {}
 
-impl<T> Vec<T> {
+impl<T> MyVec<T> {
     pub fn new() -> Self {
         assert!(mem::size_of::<T>() != 0, "We're not ready to handle ZSTs"); // if mem::size_of::<T>() == 0, panic: We're not ready to handle ZSTs
-        Vec {
+        MyVec {
             ptr: NonNull::dangling(),
             len: 0,
             cap: 0,
@@ -76,7 +78,7 @@ impl<T> Vec<T> {
     }
 }
 
-impl<T> Drop for Vec<T> {
+impl<T> Drop for MyVec<T> {
     fn drop(&mut self) {
         if self.cap !=0 {
             while let Some(_) = self.pop() { }
@@ -88,7 +90,82 @@ impl<T> Drop for Vec<T> {
     }
 }
 
+impl<T> Deref for MyVec<T> {
+    type Target = [T];
+    fn deref(&self) -> &Self::Target {
+        unsafe {
+            std::slice::from_raw_parts(self.ptr.as_ptr(), self.len)
+        }
+    }
+}
+
+impl<T> DerefMut for MyVec<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        unsafe {
+            std::slice::from_raw_parts_mut(self.ptr.as_ptr(), self.len)
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut v = MyVec::new(); // 创建一个空的 MyVec
+
+    // 添加元素
+    v.push(1);
+    v.push(2);
+    v.push(3);
+
+    // let v1 = &*v; //type v1 = &[i32]
+
+    // 打印当前的 MyVec
+    // 使用解引用来获取 MyVec 的 slice,*v 首先对 v 进行解引用，通过 Deref 特质将 v 转换为它的目标类型，即 Vec<T> 的目标类型 [T]（切片）。然后，& 对解引用得到的值再次取引用，最终结果是一个指向切片的引用。由于 [T] 类型实现了 Debug 特质，所以这里可以使用 {:?} 格式化打印切片内容。
+    println!("Current MyVec: {:?}", &*v); // Current MyVec: [1, 2, 3]
+    println!("Current MyVec: {:?}", v); // Current MyVec: MyVec { ptr: 0x250832bc770, len: 3, cap: 4 }
+
+    // 移除一个元素
+    if let Some(value) = v.pop() {
+        println!("Popped value: {}", value);
+    }
+
+    // 再次打印 MyVec 查看变化
+    println!("MyVec after pop: {:?}", &*v); // MyVec after pop: [1, 2]
+    println!("MyVec after pop: {:?}", v); // MyVec after pop: MyVec { ptr: 0x250832bc770, len: 2, cap: 4 }
+
+    // 尝试继续添加和移除元素来测试动态扩展
+    v.push(4);
+    v.push(5);
+
+    println!("MyVec after adding more elements: {:?}", &*v); // MyVec after pop: [1, 2, 4, 5]
+    println!("MyVec after adding more elements: {:?}", v); // MyVec after adding more elements: MyVec { ptr: 0x250832bc770, len: 4, cap: 4 }
+
+    // 清空 MyVec 并检查是否正确释放内存
+    while let Some(value) = v.pop() {
+        println!("Popping: {}", value);
+    }
+    
+    // 最终状态
+    println!("Final MyVec: {:?}", &*v); // Final MyVec: []
+    println!("Final MyVec: {:?}", v);  // Final MyVec: MyVec { ptr: 0x250832bc770, len: 0, cap: 4 }
+
+    let mut inter_v = MyVec::new();
+    inter_v.push("hello".to_string());
+    inter_v.push(",".to_string());
+    inter_v.push("world".to_string());
+    inter_v.push("!".to_string());
+
+    // 使用索引访问
+    //当使用 v[2] 这样的索引访问时，实际上是通过 Deref 特质自动解引用到 [T]，然后在这个切片上进行索引访问。这是因为 [] 操作符是通过 Index 或 IndexMut 特质实现的，而这些特质在 [T] 类型上已经由标准库实现。
+    // let a = inter_v[2].clone(); // type a = String
+    println!("inter_v[3]: {}", inter_v[3]);  // 直接使用索引访问，如同数组
+
+    // 直接在MyVec上迭代
+    // &*v 首先使用 * 解引用 v 到 [T] 类型的切片，然后 & 重新取得这个切片的不可变引用。这让整个结构变为 &[T]，是一个指向切片的引用，可以直接在 for 循环中迭代。
+    // 这里的解引用是通过 Deref 特质实现的，这种机制允许 MyVec<T> 的实例直接访问 [T] 上定义的方法和特质实现，包括用于迭代的 IntoIterator。
+    let mut i = 0;
+    for item in &*inter_v {       
+        println!("{i} : {}", item);
+        i = i + 1;
+    }
 
 }
