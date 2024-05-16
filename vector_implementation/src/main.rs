8d158f19c238d53b67c2f364d9e8b89e53044870
DylanJinx <2636353989@qq.com>
2024-05-16 17:08:38 +0800

    7.into_iter()_4.md

--- vector_implementation/src/main.rs
+++ vector_implementation/src/main.rs
@@ -121,7 +121,7 @@ impl<T> MyVec<T> {
 impl<T> Drop for MyVec<T> {
     fn drop(&mut self) {
         if self.cap != 0 {
-            println!("要开始释放内存咯！");
+            println!("MyVec要开始释放内存咯！");
             while let Some(_) = self.pop() { }
             let layout = Layout::array::<T>(self.cap).unwrap();
             unsafe {
@@ -179,6 +179,7 @@ impl<T> IntoIterator for MyVec<T> {
     }
 }
 
+// 向前迭代
 impl<T> Iterator for MyIntoIter<T> {
     type Item = T;
     fn next(&mut self) -> Option<T> {
@@ -200,109 +201,180 @@ impl<T> Iterator for MyIntoIter<T> {
     }
 }
 
+// 向后迭代
+impl<T> DoubleEndedIterator for MyIntoIter<T> {
+    fn next_back(&mut self) -> Option<T> {
+        if self.start == self.end {
+            None
+        } else {
+            unsafe {
+                self.end = self.end.offset(-1);
+                Some(ptr::read(self.end))
+            }
+        }
+    }
+}
+
+// MyIntoIter_Drop
+impl<T> Drop for MyIntoIter<T> {
+    fn drop(&mut self) {
+        if self.cap != 0 {
+            // 将剩下的元素drop
+            println!("MyIntoIter要开始释放内存咯！");
+            for _ in &mut *self {}
+            let layout = Layout::array::<T>(self.cap).unwrap();
+            unsafe {
+                alloc::dealloc(self.buf.as_ptr() as *mut u8, layout);
+            }
+        }
+    }
+}
+
+
 fn main() {
     println!("----------------------------------------12345. new、push、pop、drop、deref ------------------------------------");
-    let mut v = MyVec::new(); // 创建一个空的 MyVec
+    
+    {
+        println!("---------------------------------------- new、push、pop ------------------------------------");
+        let mut v = MyVec::new(); // 创建一个空的 MyVec
 
-    // 添加元素
-    v.push(1);
-    v.push(2);
-    v.push(3);
+        // 添加元素
+        v.push(1);
+        v.push(2);
+        v.push(3);
 
-    // let v1 = &*v; //type v1 = &[i32]
+        // let v1 = &*v; //type v1 = &[i32]
 
-    // 打印当前的 MyVec
-    // 使用解引用来获取 MyVec 的 slice,*v 首先对 v 进行解引用，通过 Deref 特质将 v 转换为它的目标类型，即 Vec<T> 的目标类型 [T]（切片）。然后，& 对解引用得到的值再次取引用，最终结果是一个指向切片的引用。由于 [T] 类型实现了 Debug 特质，所以这里可以使用 {:?} 格式化打印切片内容。
-    println!("Current MyVec: {:?}", &*v); // Current MyVec: [1, 2, 3]
-    println!("Current MyVec: {:?}", v); // Current MyVec: MyVec { ptr: 0x250832bc770, len: 3, cap: 4 }
+        // 打印当前的 MyVec
+        // 使用解引用来获取 MyVec 的 slice,*v 首先对 v 进行解引用，通过 Deref 特质将 v 转换为它的目标类型，即 Vec<T> 的目标类型 [T]（切片）。然后，& 对解引用得到的值再次取引用，最终结果是一个指向切片的引用。由于 [T] 类型实现了 Debug 特质，所以这里可以使用 {:?} 格式化打印切片内容。
+        println!("Current MyVec: {:?}", &*v); // Current MyVec: [1, 2, 3]
+        println!("Current MyVec: {:?}", v); // Current MyVec: MyVec { ptr: 0x250832bc770, len: 3, cap: 4 }
 
-    // 移除一个元素
-    if let Some(value) = v.pop() {
-        println!("Popped value: {}", value);
+        // 移除一个元素
+        if let Some(value) = v.pop() {
+            println!("Popped value: {}", value);
+        }
+
+        // 再次打印 MyVec 查看变化
+        println!("MyVec after pop: {:?}", &*v); // MyVec after pop: [1, 2]
+        println!("MyVec after pop: {:?}", v); // MyVec after pop: MyVec { ptr: 0x250832bc770, len: 2, cap: 4 }
+
+        // 尝试继续添加和移除元素来测试动态扩展
+        v.push(4);
+        v.push(5);
+
+        println!("MyVec after adding more elements: {:?}", &*v); // MyVec after pop: [1, 2, 4, 5]
+        println!("MyVec after adding more elements: {:?}", v); // MyVec after adding more elements: MyVec { ptr: 0x250832bc770, len: 4, cap: 4 }
+
+        // 清空 MyVec 并检查是否正确释放内存
+        while let Some(value) = v.pop() {
+            println!("Popping: {}", value);
+        }
+        
+        // 最终状态
+        println!("Final MyVec: {:?}", &*v); // Final MyVec: []
+        println!("Final MyVec: {:?}", v);  // Final MyVec: MyVec { ptr: 0x250832bc770, len: 0, cap: 4 }
+
     }
 
-    // 再次打印 MyVec 查看变化
-    println!("MyVec after pop: {:?}", &*v); // MyVec after pop: [1, 2]
-    println!("MyVec after pop: {:?}", v); // MyVec after pop: MyVec { ptr: 0x250832bc770, len: 2, cap: 4 }
+    {
+        println!("--------------------------------------- deref、drop ----------------------------------------");
+        let mut inter_v = MyVec::new();
+        inter_v.push("hello".to_string());
+        inter_v.push(",".to_string());
+        inter_v.push("world".to_string());
+        inter_v.push("!".to_string());
 
-    // 尝试继续添加和移除元素来测试动态扩展
-    v.push(4);
-    v.push(5);
+        // 使用索引访问
+        //当使用 v[2] 这样的索引访问时，实际上是通过 Deref 特质自动解引用到 [T]，然后在这个切片上进行索引访问。这是因为 [] 操作符是通过 Index 或 IndexMut 特质实现的，而这些特质在 [T] 类型上已经由标准库实现。
+        // let a = inter_v[2].clone(); // type a = String
+        println!("inter_v[3]: {}", inter_v[3]);  // 直接使用索引访问，如同数组
 
-    println!("MyVec after adding more elements: {:?}", &*v); // MyVec after pop: [1, 2, 4, 5]
-    println!("MyVec after adding more elements: {:?}", v); // MyVec after adding more elements: MyVec { ptr: 0x250832bc770, len: 4, cap: 4 }
+        // 直接在MyVec上迭代
+        // &*v 首先使用 * 解引用 v 到 [T] 类型的切片，然后 & 重新取得这个切片的不可变引用。这让整个结构变为 &[T]，是一个指向切片的引用，可以直接在 for 循环中迭代。
+        // 这里的解引用是通过 Deref 特质实现的，这种机制允许 MyVec<T> 的实例直接访问 [T] 上定义的方法和特质实现，包括用于迭代的 iter()、iter_mut()、into_iter()，因为[T]实现了IntoIterator特质。
+        let mut i = 0;
+        for item in inter_v.iter() {       
+            println!("{i} : {}", item);
+            i = i + 1;
+        }
+        println!("inter_v use iter() : {:?}", &*inter_v);
+        println!("inter_v use iter() : {:?}", inter_v);
 
-    // 清空 MyVec 并检查是否正确释放内存
-    while let Some(value) = v.pop() {
-        println!("Popping: {}", value);
+        let mut i = 0;
+        for item in inter_v.iter_mut() {       
+            println!("{i} : {}", item);
+            i = i + 1;
+        }
+        println!("inter_v use iter_mut() : {:?}", &*inter_v);
+        println!("inter_v use iter_mut() : {:?}", inter_v);
+
+        let mut i = 0;
+        for item in inter_v.into_iter() {       
+            println!("{i} : {}", item);
+            i = i + 1;
+        }
+        //println!("inter_v use into_iter() : {:?}", &*inter_v); //move
+        //println!("inter_v use into_iter() : {:?}", inter_v); //move
+
+        let mut deref_v = MyVec::new();
+        deref_v.push("hello".to_string());
+        deref_v.push(",".to_string());
+        deref_v.push("world".to_string());
+        deref_v.push("!".to_string());
+        // 直接使用&[T]
+        let mut i = 0;
+        for item in &*deref_v {       
+            println!("{i} : {}", item);
+            i = i + 1;
+        }
+
+        println!("deref_v : {:?}", &*deref_v);
+        println!("deref_v : {:?}", deref_v);
+
+        // 手动释放内存
+        drop(deref_v);
+        // println!("After drop: {:?}", &*inter_v); // inter_v 已经被 drop 了，这里会报错
+        // println!("After drop: {:?}", inter_v); // inter_v 已经被 drop 了，这里会报错
+
     }
     
-    // 最终状态
-    println!("Final MyVec: {:?}", &*v); // Final MyVec: []
-    println!("Final MyVec: {:?}", v);  // Final MyVec: MyVec { ptr: 0x250832bc770, len: 0, cap: 4 }
+    {
+        println!("-----------------------------------------------6. insert and remove -----------------------------------------------");
+        // 插入和删除元素
+        let mut v_6 = MyVec::new();
+        v_6.push(1);
+        v_6.push(2);
+        v_6.push(3);
+        v_6.push(4);
 
-    let mut inter_v = MyVec::new();
-    inter_v.push("hello".to_string());
-    inter_v.push(",".to_string());
-    inter_v.push("world".to_string());
-    inter_v.push("!".to_string());
+        println!("Before insert: {:?}", &*v_6); // Before insert: [1, 2, 3, 4]
+        println!("Before insert: {:?}", v_6); //After insert: MyVec { ptr: 0x2259113c810, len: 4, cap: 4 }
+        
+        v_6.insert(2, 10);
+        println!("After insert: {:?}", &*v_6); // After insert: [1, 2, 10, 3, 4]
+        println!("After insert: {:?}", v_6); //After insert: MyVec { ptr: 0x2259113c810, len: 5, cap: 8 }，扩容了
 
-    // 使用索引访问
-    //当使用 v[2] 这样的索引访问时，实际上是通过 Deref 特质自动解引用到 [T]，然后在这个切片上进行索引访问。这是因为 [] 操作符是通过 Index 或 IndexMut 特质实现的，而这些特质在 [T] 类型上已经由标准库实现。
-    // let a = inter_v[2].clone(); // type a = String
-    println!("inter_v[3]: {}", inter_v[3]);  // 直接使用索引访问，如同数组
-
-    // 直接在MyVec上迭代
-    // &*v 首先使用 * 解引用 v 到 [T] 类型的切片，然后 & 重新取得这个切片的不可变引用。这让整个结构变为 &[T]，是一个指向切片的引用，可以直接在 for 循环中迭代。
-    // 这里的解引用是通过 Deref 特质实现的，这种机制允许 MyVec<T> 的实例直接访问 [T] 上定义的方法和特质实现，包括用于迭代的 iter()、iter_mut()、into_iter()，因为[T]实现了IntoIterator特质。
-    let mut i = 0;
-    for item in inter_v.iter() {       
-        println!("{i} : {}", item);
-        i = i + 1;
-    }
-    let mut i = 0;
-    for item in inter_v.iter_mut() {       
-        println!("{i} : {}", item);
-        i = i + 1;
-    }
-    // let mut i = 0;
-    // for item in inter_v.into_iter() {       
-    //     println!("{i} : {}", item);
-    //     i = i + 1;
-    // }
-
-    // 直接使用&[T]
-    let mut i = 0;
-    for item in &*inter_v {       
-        println!("{i} : {}", item);
-        i = i + 1;
+        v_6.remove(1);
+        println!("After remove: {:?}", &*v_6); // After remove: [1, 10, 3, 4]
+        println!("After remove: {:?}", v_6); //After remove: MyVec { ptr: 0x2259113c810, len: 4, cap: 8 }，因为没有使用调用了如 shrink_to_fit 的方法，所以不会缩减 Vec 的内存容量
     }
 
-    println!("Before drop: {:?}", &*inter_v);
-    println!("Before drop: {:?}", inter_v);
+    {
+        println!("-----------------------------------------------7. IntoIter -----------------------------------------------");
+        let mut v_7 = MyVec::new();
+        v_7.push("hello".to_string());
+        v_7.push(",".to_string());
+        v_7.push("world".to_string());
+        v_7.push("!".to_string());
 
-    // 手动释放内存
-    drop(inter_v);
-    // println!("After drop: {:?}", &*inter_v); // inter_v 已经被 drop 了，这里会报错
-    // println!("After drop: {:?}", inter_v); // inter_v 已经被 drop 了，这里会报错
-
-    println!("-----------------------------------------------6. insert and remove -----------------------------------------------");
-    // 插入和删除元素
-    let mut v_6 = MyVec::new();
-    v_6.push(1);
-    v_6.push(2);
-    v_6.push(3);
-    v_6.push(4);
-
-    println!("Before insert: {:?}", &*v_6); // Before insert: [1, 2, 3, 4]
-    println!("Before insert: {:?}", v_6); //After insert: MyVec { ptr: 0x2259113c810, len: 4, cap: 4 }
-    
-    v_6.insert(2, 10);
-    println!("After insert: {:?}", &*v_6); // After insert: [1, 2, 10, 3, 4]
-    println!("After insert: {:?}", v_6); //After insert: MyVec { ptr: 0x2259113c810, len: 5, cap: 8 }，扩容了
-
-    v_6.remove(1);
-    println!("After remove: {:?}", &*v_6); // After remove: [1, 10, 3, 4]
-    println!("After remove: {:?}", v_6); //After remove: MyVec { ptr: 0x2259113c810, len: 4, cap: 8 }，因为没有使用调用了如 shrink_to_fit 的方法，所以不会缩减 Vec 的内存容量
+        let v_7_iter = v_7.into_iter();
+        for item in v_7_iter {
+            println!("{}", item);
+            if item == "world".to_string() {
+                break;
+            }
+        }
+    }
 
 }
